f9af470cbdfff90b54627d2ede20c7539b526185
Shaun Kimber <skimber@microsoft.com>
2021-08-13 15:03:38 -0500

    Adding Initial Project with Controller/Memory Store

---
+++ sources/FoodTrucks/src/WebApi/Controllers/FoodTrucksController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTrucks.Api
+{
+    /// <summary>
+    /// The Truck Controller.
+    /// </summary>
+    /// <remarks>This controller is used to provide HTTP endpoints to consuming clients.</remarks>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodTrucksController : ControllerBase
+    {
+        /// <summary>
+        /// Gets the Food Truck Service.
+        /// </summary>
+        private IFoodTruckService FoodTruckService { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FoodTrucksController"/> class.
+        /// </summary>
+        /// <param name="foodTruckService">The <see cref="IFoodTruckService"/>.</param>
+        public FoodTrucksController(IFoodTruckService foodTruckService)
+        {
+            FoodTruckService = foodTruckService;
+        }
+
+        /// <summary>
+        /// Creates a new food truck.
+        /// </summary>
+        /// <param name="foodTruck">The <see cref="FoodTruck"/> to create.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [HttpPut]
+        public async Task<IActionResult> CreateAsync(FoodTruck foodTruck)
+        {
+            IActionResult result;
+
+            try
+            {
+                await FoodTruckService.CreateAsync(foodTruck, default);
+                result = Ok();
+            }
+            catch (Exception e)
+            {
+                // TODO: Add Logging.
+
+                result = Problem(
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    detail: e.Message);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets a food truck for a specific location.
+        /// </summary>
+        /// <param name="locationId">The location id for the food truck.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [HttpGet("{locationId}")]
+        public async Task<IActionResult> GetAsync(int locationId)
+        {
+            IActionResult result;
+
+            try
+            {
+                var truckResult = await FoodTruckService.GetAsync(locationId, default);
+
+                if (truckResult == null)
+                {
+                    result = NotFound();
+                }
+                else
+                {
+                    result = Ok(truckResult);
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO: Add Logging.
+
+                result = Problem(
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    detail: e.Message);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ sources/FoodTrucks/src/WebApi/Models/FoodTruck.cs
@@ -0,0 +1,24 @@
+﻿namespace FoodTrucks.Api
+{
+    /// <summary>
+    /// The Food Truck Model.
+    /// </summary>
+    /// <remarks>Represents a single food truck.</remarks>
+    public sealed class FoodTruck
+    {
+        /// <summary>
+        /// Gets or sets the Address.
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Block Id.
+        /// </summary>
+        public int BlockId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Location Id.
+        /// </summary>
+        public int LocationId { get; set; }
+    }
+}
